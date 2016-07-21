f4f52c73cae6b60957a72c7060ecafed39ac7d2d
feizheng <1290657123@qq.com>
2016-07-22 00:21:06 +0800

    update apache-dev positions

--- src/initial_config.sh
+++ src/initial_config.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /private/etc
-sudo mv apache2/ ~/git-oschina/apache-dev/src/
-sudo ln -s ~/git-oschina/apache-dev/src/apache2/ /private/etc/apache2
+sudo mv apache2/ ~/github/apache-dev/src/
+sudo ln -s ~/github/apache-dev/src/apache2/ /private/etc/apache2
