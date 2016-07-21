61c07cc079784d43696ac547c4f02d280557f857
feizheng <1290657123@qq.com>
2016-07-22 00:28:32 +0800

    remove dangerous bash

--- src/initial_config.sh
+++ src/initial_config.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd /private/etc
-sudo mv apache2/ ~/github/apache-dev/src/
+# sudo mv apache2/ ~/github/apache-dev/src/
+# sudo rm -rf apache2
 sudo ln -s ~/github/apache-dev/src/apache2/ /private/etc/apache2
