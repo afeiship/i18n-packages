a1b6e038e5f9727857a3b765f11a5cf2a57ebf26
afeiship <1290657123@qq.com>
2023-08-16 22:47:20 +0800

    feat: init impl

--- packages/i18n-language-detect/src/index.ts
+++ packages/i18n-language-detect/src/index.ts
@@ -1,8 +1,51 @@
 declare var wx: any;
 
-const I18nLanguageDetect = (): void => {
-  console.log('hello');
+interface I18nLanguageDetectOptions {
+  languageKeys?: string[];
+  cacheNs?: string;
+  cacheKey?: string;
+}
+
+const defaults = {
+  languageKeys: ['language', 'lang'],
+  cacheNs: '',
+  cacheKey: 'i18next.lang',
 };
+
+class I18nLanguageDetect {
+  public static readonly type = 'languageDetector';
+  public options: I18nLanguageDetectOptions;
+  public services: any;
+
+  get cacheKey() {
+    const { cacheNs, cacheKey } = this.options;
+    const { host } = window.location;
+    const ns = cacheNs || host;
+    return `${ns}@${cacheKey}`;
+  }
+
+  constructor(services: any, inOptions: I18nLanguageDetectOptions) {
+    this.services = services;
+    this.options = { ...defaults, ...inOptions };
+  }
+
+  detect() {
+    const url = window.location.href;
+    const uri = new URL(url);
+    const { languageKeys } = this.options;
+    let lang = null;
+    for (let index = 0; index < languageKeys!.length; index++) {
+      const key = languageKeys![index];
+      const lang = uri.searchParams.get(key);
+      if (lang) break;
+    }
+    return lang || localStorage.getItem(this.cacheKey) || navigator.language;
+  }
+
+  cacheUserLanguage(lng: string) {
+    localStorage.setItem(this.cacheKey, lng);
+  }
+}
 
 // for commonjs es5 require
 if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
