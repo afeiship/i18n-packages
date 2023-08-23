89777dbb754f4d5ace582558dc2132461a54639f
afeiship <1290657123@qq.com>
2023-08-23 18:34:31 +0800

    fix: rm unsued raw provider

--- packages/react-ant-i18n/src/main.ts
+++ packages/react-ant-i18n/src/main.ts
@@ -1,9 +1,8 @@
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
 import LocaleProvider from './components/locale-provider';
-import RawLocaleProvider from './components/raw-locale-provider';
 import init4backend from './components/mode/init4backend';
 import init4memory from './components/mode/init4memory';
 
 export const useIntl = useTranslation;
-export { LocaleProvider, RawLocaleProvider, i18next, init4memory, init4backend };
+export { LocaleProvider, i18next, init4memory, init4backend };
