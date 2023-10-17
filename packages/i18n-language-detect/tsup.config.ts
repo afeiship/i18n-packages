bd07d9ca792d57b53cbcd4ec29c35fe9a548baf2
afeiship <1290657123@qq.com>
2023-10-17 10:03:14 +0800

    fix: rm global name

--- packages/i18n-language-detect/tsup.config.ts
+++ packages/i18n-language-detect/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/*.ts'],
   format: ['cjs'],
-  globalName: 'i18nHelper',
   target: 'es5',
   clean: true,
   dts: true,
