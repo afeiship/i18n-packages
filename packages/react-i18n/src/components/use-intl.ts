6210335f318651da8eafd93278ac7fd1ba187569
afeiship <1290657123@qq.com>
2024-09-21 16:10:24 +0800

    fix: upt useTranslation

--- packages/react-i18n/src/components/use-intl.ts
+++ packages/react-i18n/src/components/use-intl.ts
@@ -1,16 +1,8 @@
-import { useTranslation, UseTranslationOptions, UseTranslationResponse } from 'react-i18next';
+import { useTranslation, UseTranslationOptions } from 'react-i18next';
 
 // This is a custom hook that wraps the useTranslation hook and adds the i18n instance and the ready state to the response.
 const useIntl = (keyPrefix?: string, options?: UseTranslationOptions) => {
-  const { t, i18n, ready } = keyPrefix
-    ? useTranslation('translation', { keyPrefix, ...options })
-    : useTranslation();
-
-  return {
-    t: (key: string, options?: any) => t(key, options),
-    i18n,
-    ready
-  } as UseTranslationResponse<any>;
+  return keyPrefix ? useTranslation('translation', { keyPrefix, ...options }) : useTranslation();
 };
 
 export default useIntl;
