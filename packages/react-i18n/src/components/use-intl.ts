c8beda48a8366a41a9a1b4351b03212f4a54c519
afeiship <1290657123@qq.com>
2024-09-21 15:55:46 +0800

    fix: useIntl err

--- packages/react-i18n/src/components/use-intl.ts
+++ packages/react-i18n/src/components/use-intl.ts
@@ -2,12 +2,14 @@ import { useTranslation, UseTranslationOptions, UseTranslationResponse } from 'r
 
 // This is a custom hook that wraps the useTranslation hook and adds the i18n instance and the ready state to the response.
 const useIntl = (keyPrefix?: string, options?: UseTranslationOptions) => {
-  const { t, i18n, ready } = useTranslation('translation', { keyPrefix, ...options });
+  const { t, i18n, ready } = keyPrefix
+    ? useTranslation('translation', { keyPrefix, ...options })
+    : useTranslation();
 
   return {
     t: (key: string, options?: any) => t(key, options),
     i18n,
-    ready,
+    ready
   } as UseTranslationResponse<any>;
 };
 
