d276a690384cdecf8e9e91b05032257f1f9e9dbc
aric.zheng <1290657123@qq.com>
2025-07-28 08:59:49 +0800

    fix: mismatch pkg error

--- packages/react-ant-i18n/src/components/use-intl.ts
+++ packages/react-ant-i18n/src/components/use-intl.ts
@@ -2,6 +2,7 @@ import { useTranslation, UseTranslationOptions } from 'react-i18next';
 
 // This is a custom hook that wraps the useTranslation hook and adds the i18n instance and the ready state to the response.
 const useIntl = (keyPrefix?: string, options?: Omit<UseTranslationOptions, 'ns'>) => {
+  // @ts-ignore
   return keyPrefix ? useTranslation('translation', { keyPrefix, ...options }) : useTranslation();
 };
 
